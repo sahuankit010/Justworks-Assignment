dfb1361ff0265922f2c9748be4f1f3eaea22e563
sahuankit010 <sahu.ankit010@gmail.com>
2022-12-26 20:38:52 -0600

    adding final code, testing is left

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Justworks-Assignment
+
+1. Install Python3 ( 3.10.x ) on mac/windows devices
+
+   macOS devices -> https://docs.python-guide.org/starting/install3/osx/
+
+   windows devices -> https://www.python.org/downloads/windows/
+2. Adding python in windows / macOS after installing
+
+   1. Windows -> https://realpython.com/add-python-to-path/
+   2. macOS use this command in terminal
+
+      1. if using zsh->
+
+   ```
+   echo "alias python=/usr/bin/python3" >> ~/.zshrc
+   ```
+
+   2. if using bash ->
+
+   ```
+   echo "alias python=/usr/bin/python3" >> ~/.bashrc
+   ```
+
+    restart the terminal after setting the environment variables in windows or entering the command in macOS
+
+3. the csv file and ankit.py file location should be in the same directory.
+4. csv file name should be "data_jw.csv"
+5. Open the terminal at the location of the file.
+6. Run Command: "pip3 install pandas"
+7. Run command: "python3 ankit.py"
+8. It will generate the output.csv in the same location.
+9. In the output.csv, year is shown as MM/YY not as MM/YYYY.
--- ankit.py
+++ ankit.py
@@ -4,13 +4,17 @@ from collections import Counter
 #code starts here
 
 #importing the data file 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data_jw.csv")
+
 #removing the rows which have atleast one of the values as empty|null
 df = df.dropna()
+
+#renaming Customer Id as Customer_ID to use it later
 df.rename(columns = {'Customer Id':'Customer_ID',}, inplace = True)
 
 #dictionary for customer ids with count
 counterCustIDs = Counter(df.Customer_ID)
+
 #list of customer ids
 customer_IDs = [i for i in counterCustIDs.keys()]
 
@@ -25,93 +29,111 @@ for i in customer_IDs:
 
 #monthAndYear is month and year to be added as the list of values in customer ids
 monthAndYear=""
+
+#date list is used in the next segment of code, storing currently
 date_list={}
+
+#at the end of the loop, dict1 will have a dictionary having customer id as key and list of months for the transaction
 for i in customer_IDs:
   for j in df.values:
-    if(i==j[0]):
+    #j is list of each row (having customer id(j[0]), date(j[1]), amount (j[2]))
+    if(i == j[0]):
       if i in dict1.keys():
-        date_val=j[1]
-        str1=list(date_val.split("/"))
-        month=int(str1[0])
-        year=int(str1[-1])
-        monthAndYear=str(month)+"/"+str(year)
-        if monthAndYear+"/"+i not in date_list.keys():
-          date_list[monthAndYear+"/"+i]={}
-          date_list[monthAndYear+"/"+i][date_val]=[int(j[2])]
+        #grabbing the date in date_val variable
+        date_val = j[1]
+        str1 = list(date_val.split("/"))
+        month = int(str1[0])
+        year = int(str1[-1])
+        #monthAndYear is like "11/22"
+        monthAndYear = str(month) + "/" + str(year)
+        #logic for date with customer id
+        #date_list is storing the month+customer id as key and the date with
+        #list of transaction as the values. date will be the key for the list
+        # of the transaction on that particular date
+        if monthAndYear + "/" + i not in date_list.keys():
+          date_list[monthAndYear + "/" + i] = dict()
+          date_list[monthAndYear + "/" + i][date_val] = [int(j[2])]
         else:
-          l=[o for o in date_list[monthAndYear+"/"+i].keys()]
+          l = [o for o in date_list[monthAndYear + "/" + i].keys()]
           if date_val not in l:
-            date_list[monthAndYear+"/"+i][date_val]=[int(j[2])]
+            date_list[monthAndYear + "/" + i][date_val] = [int(j[2])]
           else:
-            date_list[monthAndYear+"/"+i][date_val].append(int(j[2]))
-        date_list[monthAndYear+"/"+i][date_val].sort()
-        date_list[monthAndYear+"/"+i][date_val]=date_list[monthAndYear+"/"+i][date_val][::-1]
+            date_list[monthAndYear + "/" + i][date_val].append(int(j[2]))
+        #logic for getting all the transaction amount in credit first
+        #and then debit(next 2 lines)
+        date_list[monthAndYear + "/" + i][date_val].sort()
+        date_list[monthAndYear + "/" + i][date_val] = date_list[monthAndYear + "/" + i][date_val][::-1]
         if monthAndYear not in dict1[i]:
           dict1[i].append(monthAndYear)
 
 dict2 = dict()
 
-for key,value in dict1.items():
-  c_id=key
+for key, value in dict1.items():
+  c_id = key
   for k in date_list.keys():
-    date_cust_id=k
-    #l=dict_of_date
-    str1=date_cust_id.split("/")
-    month=str1[0]
-    year=str1[1]
-    monthWithYear=month+"/"+year
-    id=str1[2]
-    if(c_id==id):
+    date_cust_id = k
+    #l=dictionary of date (month and year)
+    str1 = date_cust_id.split("/")
+    #extracting the month
+    month = str1[0]
+    #extracting the year
+    year = str1[1]
+    monthWithYear = month + "/" + year
+    id = str1[2]
+    if(c_id == id):
       #correct
       for m in range(len(value)):
         #value[m]=11/2022
         #initiliaze value for minimum, maximum, and ending balance
-        sum1=0
-        mini=float("inf")
-        maxi=float("-inf")
-        if(value[m]==monthWithYear):
-          key_val=dict1[key]
-          value[m]={monthWithYear:date_list[k]}
+        sum1 = 0
+        mini = float("inf")
+        maxi = float("-inf")
+        if(value[m] == monthWithYear):
+          key_val = dict1[key]
+          value[m] = {monthWithYear: date_list[k]}
           for h,p in date_list[k].items():
             #logic for finding minimum, maximum, and ending balance
             for y in p:
-              sum1+=y
-              mini=min(mini,sum1)
-              maxi=max(maxi,sum1)
-          #inserting minimum, maximum, and ending value as a list in dictionary to use later
-          dict2[c_id+"/"+monthWithYear]=[mini,maxi,sum1]
+              sum1 += y
+              mini = min(mini, sum1)
+              maxi = max(maxi, sum1)
+          #inserting minimum, maximum, and ending balance value as a list in dictionary to use later
+          dict2[c_id+ "/" + monthWithYear] = [mini, maxi, sum1]
 
 dict3 = dict()
+#creating final dictionary dict3 with customer ID as the key and value as month
+#that will be key for list of value of [minumum, maximum, ending balance]
 for i in customer_IDs:
-  dict3[i]={}
-  for key,value in dict2.items():
-    str1=key.split("/")
-    date1=str1[1]+"/"+str1[2]
-    if(i==str1[0]):
-      dict_val=dict3[i]
-      dict_val[date1]=value
+  dict3[i] = dict()
+  for key, value in dict2.items():
+    str1 = key.split("/")
+    date1 = str1[1] + "/" + str1[2]
+    if(i == str1[0]):
+      dict_val = dict3[i]
+      dict_val[date1] = value
 
-
-#creating list for all the required columns customer id, minBalance, maxBalance, endingBalance
-cust_id=[]
-date=[]
-minBalance=[]
-maxBalance=[]
-endingBalance=[]
+print(dict3)
+#creating list for all the required columns customer id, months, minBalance, maxBalance, endingBalance
+#each index in the list corresponds to the one row of customer id, minimum balance, maximum balance, and ending balance
+cust_id = []
+date = []
+minBalance = []
+maxBalance = []
+endingBalance = []
 for key,value in dict3.items():
-  for l,m in value.items():
+  #here key is customer id, and value is a list in which minimum balance, maximum balance, and ending balance are stored
+  for key1,value1 in value.items():
+    #iterating the list, at each index there are 3 values corresponds to minimum balance, maximum balance, and ending balance
     cust_id.append(key)
-    date.append(l)
-    minBalance.append(m[0])
-    maxBalance.append(m[1])
-    endingBalance.append(m[2])
-
+    date.append(key1)
+    minBalance.append(value1[0])
+    maxBalance.append(value1[1])
+    endingBalance.append(value1[2])
 
 #generating output.csv with required template
-
 df1=pd.DataFrame.from_dict({"CustomerID":cust_id,
                             "MM/YYYY":date,
                             "MinBalance":minBalance,
                             "MaxBalance":maxBalance,
                             "EndingBalance":endingBalance})
-df1.to_csv("output1.csv")
+df1.to_csv("output.csv")
